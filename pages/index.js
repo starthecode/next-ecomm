61f0107121dc4ba2348f9eec501c2289d8e7c115
Rohit Jain <grapyhost@gmail.com>
2022-07-04 20:13:27 +0530

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Layout from '../components/Layout';
+import data from '../utils/data';
+import ProductItem from '../components/productItem';
 
 export default function Home() {
-  return <Layout title="HomePage">Homepage</Layout>;
+  return (
+    <Layout title="HomePage">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
