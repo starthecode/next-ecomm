8f13c7540c629352ccd9f943e22e2659d9577f09
Rohit Jain <grapyhost@gmail.com>
2022-07-04 19:20:04 +0530

    second commit

---
+++ components/Layout.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title : 'Ecommerce App'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-extrabold">amazona</a>
+            </Link>
+            <div className="">
+              <Link href="/cart">
+                <a className="p-2">Cart</a>
+              </Link>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+
+        <main className="container m-auto mt-4 px-4">{children}</main>
+
+        <footer className="flex justify-center items-center h-10 shadow-inner">
+          <p>Copyright C 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+}
